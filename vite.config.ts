3c7f76eb50622735d528d48e609f8d2ca13626bd
ahadalshahrani4-create <ahadalshahrani4@gmail.com>
2025-12-11 13:29:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+//import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -15,10 +15,11 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
-      resolve: {
+     /* resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
       }
+      */
     };
 });
